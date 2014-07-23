6a42d95b0bdc637a773e2311da4f239ea9f54db9
Stephane Bersier <stephane.bersier@gmail.com>
2014-07-23 17:34:21 +0200

    changed implementation of DefaultGameState

--- breakthrough/game/DefaultGameState.java
+++ breakthrough/game/DefaultGameState.java
@@ -15,6 +15,11 @@ class DefaultGameState extends AbstractGameState {
     private final int size;
     private final Color[][] board;
 
+    /**
+     * whether black and white are inverted
+     */
+    private boolean invertedColors = false;
+
     DefaultGameState(Color[][] board) {
         this.size = board.length;
         this.board = copyBoard(board);
@@ -32,7 +37,19 @@ class DefaultGameState extends AbstractGameState {
 
     @Override
     public Color getColorAt(int i, int j) {
-        return board[i][j];
+        final Color color = board[get(i)][get(j)];
+        return invertedColors ? color.opposite() : color;
+    }
+
+    private void removePawnAt(int i, int j) {
+        board[get(i)][get(j)] = Color.None;
+    }
+    private void putPawnAt(int i, int j) {
+        board[get(i)][get(j)] = invertedColors ? Color.Black : Color.White;
+    }
+
+    private int get(int i) {
+        return invertedColors ? size-1 - i : i;
     }
 
     @Override
@@ -42,27 +59,12 @@ class DefaultGameState extends AbstractGameState {
 
     @Override
     public GameState after(WhiteMove move) {
-
-        // reverse the board
-        final Color[][] board = reverse(this.board);
-
-        // apply the reverse of the move on the reversed board, which is equivalent to the original move
-        board[size-1 - move.i]   [size-1 - move.j]    = Color.None;
-        board[size-1 - move.newi][size-1 - move.newj] = Color.Black;
-
-        // wrap the board
-        return new DefaultGameState(board);
-    }
-
-    private static Color[][] reverse(Color[][] board) {
-        final int size = board.length;
-        final Color[][] reverse = new Color[size][size];
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                reverse[size-1 - i][size-1 - j] = board[i][j].opposite();
-            }
-        }
-        return reverse;
+        final DefaultGameState nextState = new DefaultGameState(board);
+        nextState.invertedColors =   invertedColors;
+        nextState.removePawnAt(move.i   , move.j   );
+        nextState.putPawnAt   (move.newi, move.newj);
+        nextState.invertedColors = ! invertedColors;
+        return nextState;
     }
 
     private static Color[][] copyBoard(Color[][] board) {
